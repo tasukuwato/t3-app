0d80d9b6ab14585cbe8d9befbfec246b9fd703b4
tasukuwato <221205191@ccmailg.meijo-u.ac.jp>
2024-04-19 01:31:39 +0900

    add: navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Navigation from "@/components/auth/Navigation"
+import { getAuthSession } from "@/lib/nextauth"
+import AuthProvider from "@/components/providers/AuthProvider"
+import TrpcProvider  from "@/components/providers/TrpcProvider"
+import ToastProvider from "@/components/providers/ToastProvider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,11 +20,18 @@ interface RootLayoutProps {
 
 // レイアウト
 const RootLayout = async ({ children }: RootLayoutProps) => {
+
+   // 認証情報取得
+  const user = await getAuthSession()
+
   return (
     <html lang="ja">
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col">
-          <Navigation />
+          <AuthProvider>
+            <TrpcProvider>
+              <Navigation user={user} />
+              <ToastProvider />
 
           <main className="container mx-auto max-w-screen-md flex-1 px-2">
             {children}
@@ -28,18 +39,20 @@ const RootLayout = async ({ children }: RootLayoutProps) => {
 
           {/* フッター */}
           <footer className="py-5">
-            <div className="text-center text-sm">
-              Copyright © All rights reserved |{" "}
-              <a
-                href="https://www.youtube.com/@fullstackchannel"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                FullStackChannel
-              </a>
-            </div>
-          </footer>
+        <div className="text-center text-sm">
+          Copyright © All rights reserved |{" "}
+                  <a
+                    href="https://www.youtube.com/@fullstackchannel"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline"
+                  >
+                    FullStackChannel
+                  </a>
+                </div>
+              </footer>
+            </TrpcProvider>
+          </AuthProvider>
         </div>
       </body>
     </html>
